14ae70e1c5ad4d618ddffa0be7e114c7e7823755
Cas Cremers <cas.cremers@gmail.com>
2014-08-26 16:50:53 +0100

    Tell build scripts to respect MAKEFLAGS environment variable.
    In practice this can be used with
    
    export MAKEFLAGS="-j8"
    
    to tell make to try and use 8 cores in parallel, if possible. This can significantly
    improve compilation times.

--- src/subbuild-unix-w32.sh
+++ src/subbuild-unix-w32.sh
@@ -4,7 +4,7 @@
 CMFLAGS="-D CMAKE_BUILD_TYPE:STRING=Release"
 
 # Make for windows and linux
-cmake $CMFLAGS -D TARGETOS=Win32 . && make
+cmake $CMFLAGS -D TARGETOS=Win32 . && make $MAKEFLAGS
 
 echo
 echo
